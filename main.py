4ef6af949a9b42d498ac1f2ca494700aae0508d5
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2024-10-01 17:44:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 
-with open('quiz-questions/1vs1201.txt', "r", encoding="KOI8-R") as my_file:
+with open('quiz-questions/1vs1201_.txt', "r", encoding="KOI8-R") as my_file:
   file_contents = my_file.read()
-print(file_contents)
+# print(file_contents)
+
+letters = "⌡"
+rawdata = letters.encode("KOI8-R")
+# print(rawdata)
+
+
+def f1():
+   with open('quiz-questions/1vs1201_.txt', "r", encoding="KOI8-R") as my_file:
+     for line in my_file:
+       print(line+'1')
+      # my_str = line
+      # simvols = ['/', '-', '(', ')', 'х ф', '.', ',', '"', "'", '?', '!', 'х ф', 'м ф', 'д ф', 'к п', 'т п', 'т ф',
+      #            'к ф', 'ф сп', 'науч поп ф', 'т спек', 'ф концерт', 'т с', 'ф к', 'к с им Горького', 'Культура ФГУП',
+      #            '…', ' ПД', '_', 'Острова', '№', 'Т программа', 'Документальные фильмы', 'озвуч', 'вып', 'серия',
+      #            'выпуск', 'Сезон',
+      #            'сезон', 'часть', 'фильм', 'озв ', 'уск ']
+      # for simvol in simvols:
+      #   my_str = my_str.replace(simvol, ' ')
+      #   if "  " in my_str:
+      #     my_str = my_str.replace('  ', ' ')
+      #   sanitize_my_str = sanitize_filename(my_str.strip())
+      # sanitize.write(sanitize_my_str + '\n')
+
+f1()
