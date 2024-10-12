c9c966bc4a34a742cde1625886a1ead8d93254d1
DevmanYur <devmanyur@gmail.com>
2024-10-12 12:28:23 +0300

    up

--- tg.py
+++ tg.py
@@ -1,10 +1,13 @@
 import logging
 import os
 
+import telegram
 from dotenv import load_dotenv
-from telegram import Update, ForceReply
+from telegram import Update, ForceReply, InlineKeyboardMarkup, bot
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
+from typing import Union, List
+from telegram import InlineKeyboardButton
 
 logger = logging.getLogger(__name__)
 
@@ -16,11 +19,34 @@ def get_command_start_tg(update: Update, context: CallbackContext) -> None:
         reply_markup=ForceReply(selective=True),
     )
 
+def get_answer1(
+    buttons: List[InlineKeyboardButton],
+    n_cols: int,
+    header_buttons: Union[InlineKeyboardButton, List[InlineKeyboardButton]]=None,
+    footer_buttons: Union[InlineKeyboardButton, List[InlineKeyboardButton]]=None
+) -> List[List[InlineKeyboardButton]]:
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    if header_buttons:
+        menu.insert(0, header_buttons if isinstance(header_buttons, list) else [header_buttons])
+    if footer_buttons:
+        menu.append(footer_buttons if isinstance(footer_buttons, list) else [footer_buttons])
+    return menu
+
+
 
 def get_answer(update: Update, context: CallbackContext ) -> None:
     question = update.message.text
     chat_id = update.message.chat_id
     update.message.reply_text(text=question)
+
+
+def get_answer2(update: Update, context: CallbackContext ) -> None::
+    custom_keyboard = [['top-left', 'top-right'],
+                       ['bottom-left', 'bottom-right']]
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+    bot.send_message(chat_id=update.message.chat_id,
+                     text="Custom Keyboard Test",
+                     reply_markup=reply_markup)
 
 
 
@@ -34,16 +60,10 @@ def start_tg_bot(telegram_token):
 
 
 def main():
-    logging.basicConfig(
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-    )
-
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
     load_dotenv()
-
     telegram_token = os.environ['TG_TOKEN']
-
     start_tg_bot(telegram_token)
-
 
 if __name__ == '__main__':
     main()
---
+++ tg2.py
@@ -0,0 +1,62 @@
+import os
+from datetime import datetime
+
+import requests
+import telegram
+from dotenv import load_dotenv
+import logging
+import time
+
+
+logger = logging.getLogger('Logger')
+
+
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
+def get_notification(tg_bot, chat_id):
+    text = 'https://dvmn.org/api/long_polling/'
+    tg_bot.send_message(chat_id=chat_id, text=text)
+
+
+def get_notification2(tg_bot, chat_id):
+    custom_keyboard = [['top-left', 'top-right'],
+                       ['bottom-left', 'bottom-right']]
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+    tg_bot.send_message(chat_id=chat_id,
+                     text="Custom Keyboard Test",
+                     reply_markup=reply_markup)
+
+def get_notification3(tg_bot, chat_id):
+    question = chat_id.message.text
+    chat_id = chat_id.message.chat_id
+    text = 'https://dvmn.org/api/long_polling/'
+    tg_bot.send_message(chat_id=chat_id, text=question)
+
+def main():
+    load_dotenv()
+    telegram_token = os.environ['TG_TOKEN']
+    tg_bot = telegram.Bot(token=telegram_token)
+    chat_id = tg_bot.get_updates()[0].message.from_user.id
+
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(TelegramLogsHandler(tg_bot, chat_id))
+
+    get_notification2(tg_bot, chat_id)
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
