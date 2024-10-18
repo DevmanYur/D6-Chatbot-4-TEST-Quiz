8ad3e513f6e698305ab4d02f83af0bb138ae9541
DevmanYur <devmanyur@gmail.com>
2024-10-18 22:32:09 +0300

    Update tg5.py

--- tg5.py
+++ tg5.py
@@ -4,6 +4,7 @@ import logging
 import os
 from functools import partial
 import random
+from pprint import pprint
 
 from dotenv import load_dotenv
 from telegram import Update, ForceReply, ReplyKeyboardMarkup
@@ -83,6 +84,7 @@ def help_command(update: Update, context: CallbackContext) -> None:
 
 def echo_tg(questions_dict , update: Update, context: CallbackContext):
     q = update.message.text
+    dict_up = questions_dict
     if q == 'Новый вопрос':
 
 
@@ -98,31 +100,43 @@ def echo_tg(questions_dict , update: Update, context: CallbackContext):
 
 def main():
     questions_dict = get_questions_dict()
-    echo = partial( echo_tg, questions_dict)
+    pprint( questions_dict)
+    dict_up = questions_dict
+    print()
+    pprint(dict_up)
 
-    load_dotenv()
-    telegram_token = os.environ['TG_TOKEN']
+    random_index = random.randint(0, len(questions_dict) - 1)
+    pprint(random_index)
+    answer = questions_dict[random_index]
+    print(answer)
 
-    updater = Updater(telegram_token)
-
-    # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-
-    # on non command i.e message - echo the message on Telegram
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
-
+    dict_up.pop(random_index)
+    pprint(dict_up)
+    # echo = partial( echo_tg, questions_dict)
+    #
+    # load_dotenv()
+    # telegram_token = os.environ['TG_TOKEN']
+    #
+    # updater = Updater(telegram_token)
+    #
+    # # Get the dispatcher to register handlers
+    # dispatcher = updater.dispatcher
+    #
+    # # on different commands - answer in Telegram
+    # dispatcher.add_handler(CommandHandler("start", start))
+    # dispatcher.add_handler(CommandHandler("help", help_command))
+    #
+    # # on non command i.e message - echo the message on Telegram
+    # dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    #
+    # # Start the Bot
+    # updater.start_polling()
+    #
+    # # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # # SIGTERM or SIGABRT. This should be used most of the time, since
+    # # start_polling() is non-blocking and will stop the bot gracefully.
+    # updater.idle()
+    #
 
 if __name__ == '__main__':
     main()
